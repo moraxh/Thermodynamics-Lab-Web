e3b953c7e06fd24afcefed171d43737d56649ca2
Jorge Mora <jorgemorah671@gmail.com>
2025-04-02 19:47:36 -0600

    feat(middleware): add route protection for admin and api routes
    This commit introduces protection for `/admin` and `/api/admin` routes by redirecting unauthenticated users to the `/login` page. The changes ensure that only authenticated users with valid sessions can access these protected routes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,20 @@
 import { lucia } from "./auth";
 import { defineMiddleware } from "astro:middleware";
 
+const protectedRoutes = ["/admin", "/api/admin"]
+const redirectUrl = "/login"
+
 export const onRequest = defineMiddleware(async (context, next) => {
 	const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null;
+	const isProtectedRoute = protectedRoutes.filter((route) => context.url.pathname.startsWith(route)).length > 0
+
 	if (!sessionId) {
 		context.locals.user = null;
 		context.locals.session = null;
+
+		if (isProtectedRoute) {
+			return context.redirect(redirectUrl);
+		}
 		return next();
 	}
 
@@ -14,11 +23,19 @@ export const onRequest = defineMiddleware(async (context, next) => {
 		const sessionCookie = lucia.createSessionCookie(session.id);
 		context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
 	}
+
 	if (!session) {
 		const sessionCookie = lucia.createBlankSessionCookie();
 		context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
 	}
 	context.locals.session = session;
 	context.locals.user = user;
+
+	if (isProtectedRoute) {
+		if (!session || !user) {
+			return context.redirect(redirectUrl);
+		}
+	}
+
 	return next();
 });
