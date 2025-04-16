19f96e347bc961d9e4fb468a8a3af664a4968305
Jorge Mora <jorgemorah671@gmail.com>
2025-04-16 13:54:42 -0600

    refactor(User): remove unused imports in user API handler for cleaner code

--- src/pages/api/admin/user.ts
+++ src/pages/api/admin/user.ts
@@ -1,8 +1,4 @@
 import type { APIContext } from "astro"
-import { UserSchema } from "@db/schemas";
-import { db, User } from "astro:db"
-import { hash } from "@node-rs/argon2"
-import { passwordHashingOptions } from "../login";
 import { UserController } from "@src/controllers/UserController";
 
 export async function PATCH(context: APIContext):Promise<Response> {
