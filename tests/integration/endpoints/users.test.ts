51803ea36765f3a9ba38c2d4e0e61908efc87269
Jorge Mora <jorgemorah671@gmail.com>
2025-05-06 11:39:30 -0600

    refactor(tests): streamline user update tests by creating reusable context and form data functions

--- tests/integration/endpoints/users.test.ts
+++ tests/integration/endpoints/users.test.ts
@@ -9,6 +9,8 @@ import { passwordHashingOptions } from '@src/services/UserService';
 import type { APIContext } from 'astro';
 import type { UserSelect } from '@src/repositories/UserRepository';
 import { lucia } from '@src/auth';
+import { copyFormData } from '@src/utils/formData';
+import { V } from 'vitest/dist/chunks/reporters.d.79o4mouw.js';
 
 vi.mock('@src/repositories/UserRepository')
 vi.mock('lucia', () => import('@__mocks__/modules/lucia'))
@@ -24,20 +26,24 @@ const mockUser: UserSelect = {
 }
 
 describe('PATCH /users', async() => {
-  it('should update a user', async() => {
-    vi.spyOn(UserRepository,  'updateUser').mockResolvedValue()
+  const validFormData = new FormData()
+  validFormData.append('username', 'testuser')
+  validFormData.append('password', 'testpassword')
+  validFormData.append('confirmPassword', 'testpassword')
 
-    const formData = new FormData()
-    formData.append('username', 'testuser')
-    formData.append('password', 'testpassword')
-    formData.append('confirmPassword', 'testpassword')
-
-    const context = createMockContext(
+  const createValidContext = (formData: FormData): APIContext => {
+    return createMockContext(
       new Request('http://localhost:3000/api/users', {
         method: 'PATCH',
         body: formData
       })
-    )
+    ) 
+  }
+
+  it('should update a user', async() => {
+    vi.spyOn(UserRepository,  'updateUser').mockResolvedValue()
+
+    const context = createValidContext(validFormData)
 
     const response = await PATCH(context)
     expect(response.status).toBe(200)
@@ -48,11 +54,6 @@ describe('PATCH /users', async() => {
   it('should return an error if the params are not given', async () => {
     vi.spyOn(UserRepository,  'updateUser').mockResolvedValue()
 
-    const validFormData = new FormData()
-    validFormData.append('username', 'testuser')
-    validFormData.append('password', 'testpassword')
-    validFormData.append('confirmPassword', 'testpassword')
-
     const testCases = {
       "username": "El nombre de usuario es requerido",
       "password": "La contraseña es requerida",
@@ -60,18 +61,10 @@ describe('PATCH /users', async() => {
     }
 
     await Promise.all(Object.entries(testCases).map(async ([key, errorMessage]) => {
-      var formData = new FormData()
-      for (const [k, v] of validFormData.entries()) {
-        formData.append(k, v);
-      }
-
+      const formData = copyFormData(validFormData)
       formData.delete(key)
-      const context = createMockContext(
-        new Request('http://localhost:3000/api/users', {
-          method: 'PATCH',
-          body: formData
-        })
-      )
+
+      const context = createValidContext(formData)
 
       const response = await PATCH(context)
       expect(response.status).toBe(400)
@@ -83,17 +76,10 @@ describe('PATCH /users', async() => {
   it('should return an error if the password and the password confirmation are not the same', async () => {
     vi.spyOn(UserRepository,  'updateUser').mockResolvedValue()
 
-    const formData = new FormData()
-    formData.append('username', 'testuser')
-    formData.append('password', 'testpassword')
-    formData.append('confirmPassword', 'notthesamepassword')
+    const formData = copyFormData(validFormData)
+    formData.set('confirmPassword', 'notthesamepassword')
 
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/users', {
-        method: 'PATCH',
-        body: formData
-      })
-    )
+    const context = createValidContext(formData)
 
     const response = await PATCH(context)
     expect(response.status).toBe(400)
@@ -103,11 +89,6 @@ describe('PATCH /users', async() => {
 
   it('should return an error if the credentials are not valid', async () => {
     vi.spyOn(UserRepository,  'updateUser').mockResolvedValue()
-
-    const validFormData = new FormData()
-    validFormData.append('username', 'testuser')
-    validFormData.append('password', 'testpassword')
-    validFormData.append('confirmPassword', 'testpassword')
 
     const testCases = {
       "username": {
@@ -127,57 +108,40 @@ describe('PATCH /users', async() => {
     }
 
     await Promise.all(Object.entries(testCases).map(async ([key, info]) => {
-      var formData = new FormData()
-      for (const [k, v] of validFormData.entries()) {
-        formData.append(k, v);
-      }
-
       // Min
-      formData.set(key, 'a'.repeat(info.limits[0] - 1))
-      formData.set('confirmPassword', formData.get('password') as string)
-      var context = createMockContext(
-        new Request('http://localhost:3000/api/users', {
-          method: 'PATCH',
-          body: formData
-        })
-      )
+      await (async () => {
+        const formData = copyFormData(validFormData)
+        formData.set(key, 'a'.repeat(info.limits[0] - 1))
+        formData.set('confirmPassword', formData.get('password') as string)
 
-      var response = await PATCH(context)
-      expect(response.status).toBe(400)
-      var data = await response.json()
-      expect(data.message).toBe(info.errorMessages.min)
+        const context = createValidContext(formData)
+
+        var response = await PATCH(context)
+        expect(response.status).toBe(400)
+        var data = await response.json()
+        expect(data.message).toBe(info.errorMessages.min)
+      })()
 
       // Max
-      formData.set(key, 'a'.repeat(info.limits[1] + 1))
-      formData.set('confirmPassword', formData.get('password') as string)
-      var context = createMockContext(
-        new Request('http://localhost:3000/api/users', {
-          method: 'PATCH',
-          body: formData
-        })
-      )
+      await (async () => {
+        const formData = copyFormData(validFormData)
+        formData.set(key, 'a'.repeat(info.limits[1] + 1))
+        formData.set('confirmPassword', formData.get('password') as string)
 
-      var response = await PATCH(context)
-      expect(response.status).toBe(400)
-      var data = await response.json()
-      expect(data.message).toBe(info.errorMessages.max)
+        const context = createValidContext(formData)
+
+        var response = await PATCH(context)
+        expect(response.status).toBe(400)
+        var data = await response.json()
+        expect(data.message).toBe(info.errorMessages.max)
+      })()
     }))
   })
 
   it('should return an error if something goes wrong', async () => {
     vi.spyOn(UserRepository,  'updateUser').mockRejectedValueOnce(new Error("Error"))
 
-    const formData = new FormData()
-    formData.append('username', 'testuser')
-    formData.append('password', 'testpassword')
-    formData.append('confirmPassword', 'testpassword')
-
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/users', {
-        method: 'PATCH',
-        body: formData
-      })
-    )
+    const context = createValidContext(validFormData)
 
     const response = await PATCH(context)
     expect(response.status).toBe(500)
@@ -198,19 +162,23 @@ describe('GET /login', async() => {
 })
 
 describe('POST /login', async() => {
-  it('should login a user', async() => {
-    vi.spyOn(UserRepository, 'findUserByUsername').mockResolvedValueOnce(mockUser)
+  const validFormData = new FormData()
+  validFormData.append('username', 'testuser')
+  validFormData.append('password', 'testpassword')
 
-    const formData = new FormData()
-    formData.append('username', 'testuser')
-    formData.append('password', 'testpassword')
-
-    const context = createMockContext(
+  const createValidContext = (formData: FormData): APIContext => {
+    return createMockContext(
       new Request('http://localhost:3000/api/login', {
         method: 'POST',
         body: formData
       })
-    )
+    ) 
+  }
+
+  it('should login a user', async() => {
+    vi.spyOn(UserRepository, 'findUserByUsername').mockResolvedValueOnce(mockUser)
+
+    const context = createValidContext(validFormData)
 
     const response = await LoginPOST(context)
     expect(response.status).toBe(200)
@@ -221,28 +189,16 @@ describe('POST /login', async() => {
   it('should return an error if the user or password is not given', async () => {
     vi.spyOn(UserRepository, 'findUserByUsername').mockResolvedValueOnce(mockUser)
 
-    const validFormData = new FormData()
-    validFormData.append('username', 'testuser')
-    validFormData.append('password', 'testpassword')
-
     const testCases = {
       "username": "El nombre de usuario es requerido",
       "password": "La contraseña es requerida",
     }
 
     await Promise.all(Object.entries(testCases).map(async ([key, errorMessage]) => {
-      var formData = new FormData()
-      for (const [k, v] of validFormData.entries()) {
-        formData.append(k, v);
-      }
-
+      const formData = copyFormData(validFormData)
       formData.delete(key)
-      const context = createMockContext(
-        new Request('http://localhost:3000/api/login', {
-          method: 'POST',
-          body: formData
-        })
-      )
+
+      const context = createValidContext(formData)
 
       const response = await LoginPOST(context)
       expect(response.status).toBe(400)
@@ -254,16 +210,7 @@ describe('POST /login', async() => {
   it('should return an error if user does not exist', async () => {
     vi.spyOn(UserRepository, 'findUserByUsername').mockResolvedValueOnce(null)
 
-    const formData = new FormData()
-    formData.append('username', 'testuser')
-    formData.append('password', 'testpassword')
-
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/login', {
-        method: 'POST',
-        body: formData
-      })
-    )
+    const context = createValidContext(validFormData)
 
     const response = await LoginPOST(context)
     expect(response.status).toBe(400)
@@ -274,16 +221,7 @@ describe('POST /login', async() => {
   it('should return an error if password is incorrect', async () => {
     vi.spyOn(UserRepository, 'findUserByUsername').mockResolvedValueOnce(mockUser)
 
-    const formData = new FormData()
-    formData.append('username', 'testuser')
-    formData.append('password', 'notthesamepassword')
-
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/login', {
-        method: 'POST',
-        body: formData
-      })
-    )
+    const context = createValidContext(validFormData)
 
     const response = await LoginPOST(context)
     expect(response.status).toBe(400)
@@ -294,16 +232,7 @@ describe('POST /login', async() => {
   it('should return an error if something goes wrong', async () => {
     vi.spyOn(UserRepository, 'findUserByUsername').mockRejectedValueOnce(new Error("Error"))
 
-    const formData = new FormData()
-    formData.append('username', 'testuser')
-    formData.append('password', 'notthesamepassword')
-
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/login', {
-        method: 'POST',
-        body: formData
-      })
-    )
+    const context = createValidContext(validFormData)
 
     const response = await LoginPOST(context)
     expect(response.status).toBe(500)
@@ -324,16 +253,19 @@ describe('GET /logout', async() => {
 })
 
 describe('POST /logout', async() => {
-  it('should logout a user', async () => {
+  const createValidContext = (): APIContext => {
     const context = createMockContext(
       new Request('http://localhost:3000/api/logout', {
         method: 'POST',
-        headers: {
-          'auth_session': 'session-id'
-        }
       })
     )
     context.cookies.set('auth_session', 'session-id')
+
+    return context
+  }
+
+  it('should logout a user', async () => {
+    const context = createValidContext()
 
     const response = await LogoutPOST(context)
     expect(response.status).toBe(200)
@@ -342,14 +274,8 @@ describe('POST /logout', async() => {
   })
 
   it('should return an error if the auth session is not given', async () => {
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/logout', {
-        method: 'POST',
-        headers: {
-          'auth_session': 'session-id'
-        }
-      })
-    )
+    const context = createValidContext()
+    context.cookies.delete('auth_session')
 
     const response = await LogoutPOST(context)
     expect(response.status).toBe(400)
@@ -360,15 +286,7 @@ describe('POST /logout', async() => {
   it('should return an error if the auth session is not found', async () => {
     vi.spyOn(lucia, 'validateSession').mockResolvedValueOnce({ user: null, session: null })
 
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/logout', {
-        method: 'POST',
-        headers: {
-          'auth_session': 'session-id'
-        }
-      })
-    )
-    context.cookies.set('auth_session', 'session-id')
+    const context = createValidContext()
 
     const response = await LogoutPOST(context)
     expect(response.status).toBe(400)
@@ -379,15 +297,7 @@ describe('POST /logout', async() => {
   it('should return an error if something goes wrong', async () => {
     vi.spyOn(lucia, 'validateSession').mockRejectedValueOnce(new Error("Error"))
 
-    const context = createMockContext(
-      new Request('http://localhost:3000/api/logout', {
-        method: 'POST',
-        headers: {
-          'auth_session': 'session-id'
-        }
-      })
-    )
-    context.cookies.set('auth_session', 'session-id')
+    const context = createValidContext()
 
     const response = await LogoutPOST(context)
     expect(response.status).toBe(500)
